e50204bb962c92b8d007e238c5d8c2c5e661970a
John Peart <code@johnpe.art>
2020-06-20 14:46:01 +0100

    Create keys-template.py

---
+++ keys-template.py
@@ -0,0 +1,12 @@
+############################################################
+## RENAME THIS FILE TO 'keys.py' ONCE YOU'VE FILLED IT IN ##
+############################################################
+# Populate your twitter API details below, replacing
+# CONSUMER_KEY_HERE etc with your details from Twitter
+
+twitter = {
+  "consumer_key": "your_consumer_key",
+  "consumer_secret": "your_consumer_secret",
+  "access_token_key": "your_access_token_key",
+  "access_token_secret": "your_access_token_secret"
+}
